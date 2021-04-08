a04b5e0bdf74c7fa5af3e7521eda952037defaa7
wuxianlin <wuxianlinwxl@gmail.com>
2021-04-09 00:19:34 +0200

    mt8163-common: Build MediaTek Sensors HIDL HAL from source
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>

--- mt8163.mk
+++ mt8163.mk
@@ -234,8 +234,8 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl \
-    android.hardware.sensors@1.0-service
+    android.hardware.sensors@1.0-impl-mediatek \
+    android.hardware.sensors@1.0-service-mediatek
 
 # Filesystem management tools
 PRODUCT_PACKAGES += \
