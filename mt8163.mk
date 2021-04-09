0d664c0d1df40d31143dcff7332ab5b8a2b1e5d0
LuK1337 <priv.luk@gmail.com>
2021-04-09 14:38:51 +0200

    mt8163-common: Build libdrm from source
    Change-Id: I4677dde2852a4507e52b6076dca6cdf5ace419c8
    
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>

--- mt8163.mk
+++ mt8163.mk
@@ -133,6 +133,7 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service.widevine \
     android.hardware.drm@1.0-service \
+    libdrm \
     libmockdrmcryptoplugin
 
 # FM Radio
