51e971591ddde91099fb70d3d749d4a209519511
R0rt1z2 <rogerortizleal@gmail.com>
2021-04-09 11:46:44 +0200

    mt8163-common: Set MTK_BT_SUPPORT as yes
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -66,6 +66,7 @@ BOARD_CHARGER_ENABLE_SUSPEND := true
 BACKLIGHT_PATH := /sys/class/leds/lcd-backlight/brightness
 
 # Bluetooth
+MTK_BT_SUPPORT := yes
 BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_MTK := true
 BOARD_BLUETOOTH_DOES_NOT_USE_RFKILL := true
