1d18c940cbfd50d71225b4ab09c7a9ca2739ca48
OzzysCmAcc <oskarkratochvil86@yahoo.com>
2021-04-09 12:04:53 +0200

    mt8163-common: Binderize soundtrigger HIDL HAL
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>

--- mt8163.mk
+++ mt8163.mk
@@ -172,6 +172,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@2.0-impl \
     android.hardware.broadcastradio@1.0-impl \
     android.hardware.soundtrigger@2.0-impl \
+    android.hardware.soundtrigger@2.0-service \
     audio.a2dp.default \
     audio.r_submix.default \
     audio.r_submix.mt8163 \
