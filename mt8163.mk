c5bcef260b2aa2050ae70904bccd40f9bbb548c7
ThEMarD <themard85@gmail.com>
2021-04-09 15:42:14 +0200

    mt8163-common: Copy audio policies from frameworks
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>

--- mt8163.mk
+++ mt8163.mk
@@ -194,6 +194,11 @@ PRODUCT_PACKAGES += \
     tinymix
 
 PRODUCT_COPY_FILES += \
+    $(TOPDIR)frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration.xml \
+    $(TOPDIR)frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
+    $(TOPDIR)frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
+    $(TOPDIR)frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
+    $(TOPDIR)frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml \
     $(COMMON_PATH)/configs/audio/audio_effects.xml:system/etc/audio_effects.xml \
     $(COMMON_PATH)/configs/audio/audio_device.xml:system/etc/audio_device.xml \
     $(COMMON_PATH)/configs/audio/audio_em.xml:system/etc/audio_em.xml \
