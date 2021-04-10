00544552422ebb7b32e70630d827efbfbfbf64a3
R0rt1z2 <rogerortizleal@gmail.com>
2021-04-11 00:04:12 +0200

    mt8163-common: fix camera media profiles
    Signed-off-by: R0rt1z2 <rogerortizleal@gmail.com>

--- mt8163.mk
+++ mt8163.mk
@@ -192,7 +192,7 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:system/etc/media_codecs_google_video_le.xml \
-    $(COMMON_PATH)/configs/media/media_profiles.xml:system/etc/media_profiles.xml \
+    $(COMMON_PATH)/configs/media/media_profiles.xml:system/etc/media_profiles_V1_0.xml \
     $(COMMON_PATH)/configs/media/media_codecs.xml:system/etc/media_codecs.xml \
     $(COMMON_PATH)/configs/media/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
     $(COMMON_PATH)/configs/media/media_codecs_mediatek_video.xml:system/etc/media_codecs_mediatek_video.xml \
