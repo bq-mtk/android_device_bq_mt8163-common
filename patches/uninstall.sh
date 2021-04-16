58b401e9c546b50aaaa547f82ed5ac98d25a26a5
jmpfbmx <42691741+jmpfbmx@users.noreply.github.com>
2021-04-16 10:10:36 +0200

    mt8163-common: patches: Drop FMRadio patch
     * It just makes FM can't startup bcuz of the freq, without it FMRadio makes more than with it
    
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>

--- patches/uninstall.sh
+++ patches/uninstall.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 rootdirectory="$PWD"
-dirs="bionic build/make/core build/soong frameworks/av frameworks/base frameworks/native hardware/interfaces system/core system/sepolicy system/bt external/wpa_supplicant_8 packages/apps/FMRadio"
+dirs="bionic build/make/core build/soong frameworks/av frameworks/base frameworks/native hardware/interfaces system/core system/sepolicy system/bt external/wpa_supplicant_8"
 
 for dir in $dirs ; do
 	cd $rootdirectory
