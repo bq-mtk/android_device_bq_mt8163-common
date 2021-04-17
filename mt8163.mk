5d0404f92dd9c0f8ff95f84e30b80846ade757b3
jmpfbmx <42691741+jmpfbmx@users.noreply.github.com>
2021-04-18 00:14:47 +0200

    mt8163-common: Drop Soundtrigger HIDL HAL
     * BQ dropped Soundtrigger support at Oreo M10HD Stock
    
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>

--- mt8163.mk
+++ mt8163.mk
@@ -21,8 +21,6 @@ PRODUCT_PACKAGES += \
 	android.hardware.audio@2.0-service \
 	android.hardware.audio.effect@2.0-impl \
 	android.hardware.broadcastradio@1.0-impl \
-	android.hardware.soundtrigger@2.0-impl \
-	android.hardware.soundtrigger@2.0-service \
 	audio.a2dp.default \
 	audio.r_submix.default \
 	audio.r_submix.mt8163 \
