fcc20c66614ce59df44640407c606c6d300d5bf5
steadfasterX <steadfasterX@gmail.com>
2021-05-10 12:09:31 +0200

    mt8163-common: Fix Graphic Glitches
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>

--- mt8163.mk
+++ mt8163.mk
@@ -133,6 +133,9 @@ PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.vulkan.level-0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.level.xml \
 	frameworks/native/data/etc/android.hardware.vulkan.version-1_0_3.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version.xml
 
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.hwui.renderer=opengl
+
 # GPS
 PRODUCT_PACKAGES += \
 	android.hardware.gnss@1.0-impl \
