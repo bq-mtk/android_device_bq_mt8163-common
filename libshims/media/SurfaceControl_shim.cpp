cd382905dd7fa9053db7578f0bcf62dac6dab5ed
Ethan Chen <intervigil@gmail.com>
2021-05-10 12:58:54 +0200

    mt8163-common: Update SurfaceComposerClient shim
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>

--- libshims/media/SurfaceControl_shim.cpp
+++ libshims/media/SurfaceControl_shim.cpp
@@ -53,6 +53,8 @@ android::SurfaceComposerClient::Transaction *t;
 /* Android N exports */
 extern "C" {
     extern void _ZN7android14SurfaceControl8setLayerEi(int32_t) {}
+    extern void _ZN7android14SurfaceControl7destroyEv(void);
+    extern void *_ZN7android21SurfaceComposerClient23getPhysicalDisplayTokenEy(unsigned long long);
 
     void _ZN7android21SurfaceComposerClient13createSurfaceERKNS_7String8EjjijPNS_14SurfaceControlEjj(
 	    android::String8 const & s,
@@ -106,5 +108,26 @@ extern "C" {
 
     void _ZN7android21SurfaceComposerClient21openGlobalTransactionEv() {
         t = new(android::SurfaceComposerClient::Transaction);
+    }
+
+    void _ZN7android21SurfaceComposerClient20setDisplayProjectionERKNS_2spINS_7IBinderEEEjRKNS_4RectES8_(
+        const sp<IBinder>& token, uint32_t orientation, const Rect& layerStackRect, const Rect& displayRect) {
+      android::SurfaceComposerClient::Transaction t;
+      t.setDisplayProjection(token, orientation, layerStackRect, displayRect);
+      t.apply();
+    }
+
+    void _ZN7android21SurfaceComposerClient22closeGlobalTransactionEb() {
+        t->apply();
+        delete t;
+        t = nullptr;
+    }
+
+    void _ZN7android14SurfaceControl5clearEv(void) {
+        _ZN7android14SurfaceControl7destroyEv();
+    }
+
+    void* _ZN7android21SurfaceComposerClient17getBuiltInDisplayEi(int32_t id) {
+        return _ZN7android21SurfaceComposerClient23getPhysicalDisplayTokenEy(static_cast<uint64_t>(id));
     }
 }
