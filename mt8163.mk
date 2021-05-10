c8b9d304fff06378b747f77a725c1c021153c35e
DarkDampSquib <andrin.geiger1998@gmail.com>
2021-05-10 12:12:21 +0200

    mt8163-common: Enable debug.sf.latch_unsignaled
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>

--- mt8163.mk
+++ mt8163.mk
@@ -136,7 +136,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.hwui.renderer=opengl \
     debug.hwui.use_partial_updates=false \
-    debug.sf.disable_backpressure=1
+    debug.sf.disable_backpressure=1 \
+    debug.sf.latch_unsignaled=1
 
 # GPS
 PRODUCT_PACKAGES += \
