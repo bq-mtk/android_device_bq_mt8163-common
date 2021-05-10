8d3e2a1150df4d2cb4cc0415685adbfae652bbab
Ethan Chen <intervigil@gmail.com>
2021-05-10 12:54:18 +0200

    mt8163-common: Update SurfaceControl shim for Android P
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>

--- libshims/media/SurfaceControl_shim.cpp
+++ libshims/media/SurfaceControl_shim.cpp
@@ -49,11 +49,17 @@ using android::Rect;
 
 /* Android N exports */
 extern "C" {
-    extern void _ZN7android14SurfaceControl8setLayerEi(int32_t);
-    extern void* _ZN7android21SurfaceComposerClient13createSurfaceERKNS_7String8EjjijPNS_14SurfaceControlEjj(
-        const android::String8& name, uint32_t w, uint32_t h, PixelFormat format,
-        uint32_t flags, SurfaceControl* parent, uint32_t windowType,
-        uint32_t ownerUid);
+    extern void _ZN7android14SurfaceControl8setLayerEi(int32_t) {}
+
+    void _ZN7android21SurfaceComposerClient13createSurfaceERKNS_7String8EjjijPNS_14SurfaceControlEjj(
+	    android::String8 const & s,
+	    uint32_t w,
+	    uint32_t h,
+	    android::PixelFormat fmt,
+	    uint32_t flags,
+	    void *parent,
+	    uint32_t windowType,
+	    uint32_t ownerUid) {/*FIXME*/}
 
     /* 
     * FUNCTION NAME: SurfaceComposerClient::CreateSurface::SurfaceControl.
@@ -61,7 +67,7 @@ extern "C" {
     * NOTES: It looks like this function was renamed in N. Stub out to the correct call.
     */
 
-    void* _ZN7android21SurfaceComposerClient13createSurfaceERKNS_7String8EjjijPNS_14SurfaceControlEii(
+    void _ZN7android21SurfaceComposerClient13createSurfaceERKNS_7String8EjjijPNS_14SurfaceControlEii(
         const android::String8& name, // surface name.
         uint32_t w, // width in pixel
         uint32_t h, // height in pixel
@@ -72,7 +78,7 @@ extern "C" {
         uint32_t /* ownerUID */) // UID of the task
     {
         /* sp<SurfaceControl> android::SurfaceComposerClient::createSurface(android::String8 const&, unsigned int, unsigned int, int, unsigned int) */
-        return _ZN7android21SurfaceComposerClient13createSurfaceERKNS_7String8EjjijPNS_14SurfaceControlEjj(name, w, h, format, flags, nullptr, 0, 0);
+        _ZN7android21SurfaceComposerClient13createSurfaceERKNS_7String8EjjijPNS_14SurfaceControlEjj(name, w, h, format, flags, NULL, 0, 0);
     }
 
     /* 
@@ -80,9 +86,10 @@ extern "C" {
     * USE: Creates a surface.
     * NOTES: It looks like this function was renamed in N. Stub out to the correct call.
     */
-    void* _ZN7android21SurfaceComposerClient13createSurfaceERKNS_7String8Ejjij(android::String8 const& s, uint32_t w, uint32_t h,
+    void _ZN7android21SurfaceComposerClient13createSurfaceERKNS_7String8Ejjij(android::String8 const& s, uint32_t w, uint32_t h,
         android::PixelFormat fmt, uint32_t flags) {
-            return _ZN7android21SurfaceComposerClient13createSurfaceERKNS_7String8EjjijPNS_14SurfaceControlEjj(s, w, h, fmt, flags, NULL, 0, 0);
+        /* sp<SurfaceControl> android::SurfaceComposerClient::createSurface(android::String8 const&, unsigned int, unsigned int, int, unsigned int) */
+        _ZN7android21SurfaceComposerClient13createSurfaceERKNS_7String8EjjijPNS_14SurfaceControlEjj(s, w, h, fmt, flags, NULL, 0, 0);
     }
 
     /* 
