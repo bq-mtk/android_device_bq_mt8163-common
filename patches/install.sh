aa1748c8c666be6bef522c99afe1932df1251f30
Jan Altensen <info@stricted.net>
2021-05-10 13:10:53 +0200

    mt8163-common: Add Fix bt in MTK legacy devices patch
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>

--- patches/install.sh
+++ patches/install.sh
@@ -4,7 +4,7 @@ echo $1
 rootdirectory="$PWD"
 # ---------------------------------
 
-dirs="frameworks/av frameworks/base frameworks/native hardware/interfaces packages/apps/FMRadio system/core system/netd"
+dirs="frameworks/av frameworks/base frameworks/native hardware/interfaces packages/apps/FMRadio system/bt system/core system/netd"
 
 # red + nocolor
 RED='\033[0;31m'
