a64246b28448ecb00ecd49d1b29c46188e8f08d2
jmpfbmx <jmpf.bmx@gmail.com>
2021-05-10 14:24:13 +0200

    mt8163-common: Build LiveDisplay HAL
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>

--- mt8163.mk
+++ mt8163.mk
@@ -182,6 +182,10 @@ PRODUCT_PACKAGES += \
 	android.hardware.light@2.0-service-mediatek \
 	lights.mt8163
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-mediatek
+
 # Media
 PRODUCT_PACKAGES += \
 	android.hardware.media.omx@1.0
