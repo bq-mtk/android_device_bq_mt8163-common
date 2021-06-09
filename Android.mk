916f9897a022c7cc9c1950eb0f8728302b0698b5
Kra1o5 <kra1o5x@gmail.com>
2021-06-09 18:49:44 +0200

    mt8163-common: Rename to correct codename

--- Android.mk
+++ Android.mk
@@ -1,5 +1,5 @@
 LOCAL_PATH := $(call my-dir)
-ifneq ($(filter zeno freezerfhd,$(TARGET_DEVICE)),)
+ifneq ($(filter karin freezerfhd,$(TARGET_DEVICE)),)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 include $(CLEAR_VARS)
 endif
